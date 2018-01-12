c60035492cfd56f8b93da2b77816fad8f7e8defa
ivanruiz25 <ivanruizde25@gmail.com>
2018-01-12 12:56:54 +0100

    primer commit

---
+++ js/js.js
@@ -0,0 +1,47 @@
+	
+
+	//hacemos todos los elementos draggables para poder moverlos por la pantalla
+	// revert sirve para una vez soltar el objeto pueda volver a su destino 
+
+	$(".cuadrado").draggable({ revert: true});
+	$(".triangulo").draggable({revert: true});
+	$(".huevo").draggable({revert: true});
+	$(".trapecio").draggable({revert: true});
+
+	var contador=0;
+
+$(".vacio").droppable({
+
+		drop: function(event,ui){
+
+				//variables para guardar los sonidos 
+				  var bien = new Audio("sound/bien.mp3");
+				  var mal = new Audio("sound/mal.mp3");
+
+				// si los id son iguales, las figuras seran iguales 
+				if(ui.draggable.attr("id")==$(this).attr("id"))
+				{
+					//borramos las 2 figuras de la pantalla
+					$(this).remove();
+					ui.draggable.remove();
+					// sumamos el contador
+					// para saber cuantos aciertos lleva el usuario 
+					contador++;
+					bien.play();
+				}	
+				else
+				{
+					mal.play();
+					alert("LA FIGURA NO ES CORRECTA");
+
+				}
+				// si el contador es 4 el jugador habra ganado 
+				if(contador===4)
+				{
+					alert("HAS GANADO");
+					// creamos un boton para refrescar la pagina y poder volver a jugar 
+					$(".boton").html("<input type=\"submit\" value=\"Volver a jugar\" onclick=\"javascript:window.location.reload();\"/>")
+				}
+		},
+
+	});
