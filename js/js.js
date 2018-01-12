ec2e0e6a5501bc99fc67bc37060bdfa5abc26a73
ivanruiz25 <ivanruizde25@gmail.com>
2018-01-12 17:57:46 +0100

    proyecto terminado

--- js/js.js
+++ js/js.js
@@ -1,12 +1,29 @@
-	
+	function sacarFigurasAleatorias()
+	{
+	var figuras=["cuadrado","triangulo","trapecio","huevo"];
+	var figura="";
 
-	//hacemos todos los elementos draggables para poder moverlos por la pantalla
-	// revert sirve para una vez soltar el objeto pueda volver a su destino 
+	for(let i=0;i< 4;i++)
+	{
+		var posicion=Math.floor(Math.random()*(figuras.length));
+		figura+="<div class=\""+figuras[posicion]+"\"  id=\""+figuras[posicion]+"\"></div>";
+		figuras.splice(posicion,1);
+	}
+
+	$(".contenedorGrises").html(figura);
 
 	$(".cuadrado").draggable({ revert: true});
 	$(".triangulo").draggable({revert: true});
 	$(".huevo").draggable({revert: true});
 	$(".trapecio").draggable({revert: true});
+
+
+};
+
+	//hacemos todos los elementos draggables para poder moverlos por la pantalla
+	// revert sirve para una vez soltar el objeto pueda volver a su destino 
+
+
 
 	var contador=0;
 
@@ -45,3 +62,4 @@ $(".vacio").droppable({
 		},
 
 	});
+
